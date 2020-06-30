d6d3817ffb047395c04d4e99f8a1cbdfa8145f71
Steve <anil.stephen66@gmail.com>
2020-06-30 16:16:11 +0530

    Added google font

--- functions.php
+++ functions.php
@@ -7,3 +7,14 @@ add_action('wp_enqueue_scripts',function(){
 
 //Include Files
 include('lib/custom-header/header-functions.php');
+
+//Add google fonts
+add_filter( 'sp_list_google_fonts', function( $fonts ){
+
+  $fonts[] = array(
+      'slug'	=> 'rubik',
+      'name'	=> 'Rubik',
+      'url'	  => 'Rubik'
+    );
+  return $fonts;
+});
