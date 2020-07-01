35190b8b601cfc9cd92ffe3105943dcf9692f408
Steve <anil.stephen66@gmail.com>
2020-07-01 10:47:27 +0530

    Added sticky navbar

--- lib/custom-header/header-template.php
+++ lib/custom-header/header-template.php
@@ -1,24 +1,25 @@
-<div class="container" style="background-color: #fff;">
-  <div class="white-bg">
-    <a href="#" class="btn btn-blue btn-outline">TRY OUR WEB APP</a>
-    <span>or</span>
-    <a href="#" class="btn btn-blue">DOWNLOAD WORKSHEETS</a>
-    <span>Its Free!</span>
+<div class="sticky-transparent-header" data-spy="affix">
+  <div class="container-fluid sticky-transparent" style="background-color: #fff;">
+    <div class="white-bg">
+      <a href="#" class="btn btn-blue btn-outline">TRY OUR WEB APP</a>
+      <span>or</span>
+      <a href="#" class="btn btn-blue">DOWNLOAD WORKSHEETS</a>
+      <span>Its Free!</span>
+    </div>
   </div>
+  <nav class="navbar navbar-default header5">
+    <div class="container-fluid"><!-- .container-->
+      <!-- Brand and toggle get grouped for better mobile display -->
+  		<div class="navbar-header">
+  			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" aria-controls="navbar">
+  				<span class="sr-only">Toggle navigation</span>
+  				<span class="icon-bar"></span>
+  				<span class="icon-bar"></span>
+  				<span class="icon-bar"></span>
+  			</button>
+  			<?php do_action('sp_logo');?>
+  		</div>
+  		<?php do_action('sp_nav_menu');?>
+    </div><!-- /.container-->
+  </nav>
 </div>
-<nav class="navbar navbar-default header5">
-  <div class="container"><!-- .container-->
-    <!-- Brand and toggle get grouped for better mobile display -->
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<?php do_action('sp_logo');?>
-		</div>
-
-		<?php do_action('sp_nav_menu');?>
-  </div><!-- /.container-->
-</nav>
