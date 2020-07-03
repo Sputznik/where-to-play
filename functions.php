2d6fadeed22685c83e9d4c31129a6b927164a59a
unknown <samvthom16@gmail.com>
2020-07-03 19:58:35 +0530

    Blue buttons on header

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 /*ENQUEUE STYLES*/
 add_action('wp_enqueue_scripts',function(){
-  wp_enqueue_style('sp-child-css', get_stylesheet_directory_uri().'/assets/css/where-to-play.css', array('sp-core-style'), '1.0.1' );
+  wp_enqueue_style('sp-child-css', get_stylesheet_directory_uri().'/assets/css/where-to-play.css', array('sp-core-style'), '1.0.2' );
 },99);
 
 //Include Files
