6bbacb0f2ca7cfe28333e546b22932db4d723afc
Steve <anil.stephen66@gmail.com>
2020-08-05 15:30:44 +0530

    Updated articles-resources template

--- partials/post-common.php
+++ partials/post-common.php
@@ -1,4 +1,20 @@
 <?php
+
+  $title = get_the_title();
+  $title_length = strlen( $title );
+  $excerpt_length = 0;
+  if( $title_length < 120 ){
+    $excerpt_length = 120 - $title_length;
+
+    if( $excerpt_length < 15 ){
+      $excerpt_length = 15;
+    }
+    if( $excerpt_length > 30 ){
+      $excerpt_length = 30;
+    }
+  }
+?>
+<?php
   global $post;
   $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' );
   $redirect_text = get_post_meta( get_the_ID(), 'btn_text', true );
@@ -8,7 +24,9 @@
   <div class="orbit-thumbnail-bg" style="background-image: url( '<?php _e( $thumbnail[0] );?> ');"></div>
   <div class="resource-meta">
     <h5 class="title"><?php the_title();?></h5>
-    <div class="desc"><?php _e( do_shortcode( '[orbit_excerpt]' ) );?></div>
+    <div class="desc">
+      <?php echo excerpt( $excerpt_length ); ?>
+    </div>
   </div>
   <a class="btn btn-redirect" href="<?php _e( $redirect_text );?>" target="_blank">
     <?php _e( $redirect_text );?>
