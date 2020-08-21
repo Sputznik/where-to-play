1565265e1789a855b30bdfa7d8287516e49643fd
psychome004 <anil.stephen66@gmail.com>
2020-08-21 14:02:43 +0530

    Updated filter terms order

--- lib/filters/class-play-filters-shortcode-nested-filters.php
+++ lib/filters/class-play-filters-shortcode-nested-filters.php
@@ -30,16 +30,45 @@
 		function get_terms( $taxonomy ){
 
 			$terms = array();
+			$updated_terms = array();
+
+			// TERMS CUSTOM ORDER
+			$roles_order = ['entrepreneurs', 'business-managers','educators', 'consultants' ];
+
+			$resource_types_order = ['worksheets', 'book', 'case-studies', 'online-training', 'teaching-materials', 'research', 'blog'];
+
 			if( $taxonomy ){
 
 				$terms = get_terms( array(
 			    'taxonomy' => $taxonomy,
 			    'hide_empty' => false,
 				));
+
+				$order = $taxonomy === 'roles' ? $roles_order : $resource_types_order;
+
+				$updated_terms = $this->change_terms_order( $order, $terms );
 			}
 
-			return $terms;
+			return $updated_terms;
+
 		}
+
+		// CHANGES THE DEFAULT ORDER OF TERMS
+		function change_terms_order( $order, $terms ){
+
+			$updated_terms = $terms;
+
+			usort( $updated_terms, function ( $a, $b ) use ( $order ) {
+		    $pos_a = array_search( $a->slug, $order );
+		    $pos_b = array_search( $b->slug, $order );
+				return $pos_a - $pos_b;
+			});
+
+			return $updated_terms;
+
+		}
+
+
 
 		function print_terms_list( $taxonomy, $list_class, $filter_type, $global_text = '' ){
 
