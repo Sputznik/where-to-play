e8d341263027d70c333c8f015eb5b5c7edc4d3ad
psychome004 <anil.stephen66@gmail.com>
2020-12-21 13:37:57 +0530

    Updated archive-template

--- archive.php
+++ archive.php
@@ -1,10 +1,10 @@
 <?php get_header();
   $term = $wp_query->get_queried_object();;
 ?>
-  <div class="container-fluid" style="padding-top: 35px; padding-bottom: 35px;">
+  <div class="container-fluid post-archive">
     <div class="row">
       <div class="col-sm-8">
-        <h1 class="archive-title" style="text-transform: capitalize;margin-bottom: 30px;color: #1f2939;">
+        <h1 class="archive-title">
             Tagged Under: <?php echo $term->name; ?>
         </h1>
         <?php if( have_posts() ): ?>
@@ -16,24 +16,29 @@
             <?php endwhile;?>
           </ul>
         <?php endif;?>
-        <div class="search-pagination">
-          <div class=" text-center">
-            <?php
-              the_posts_pagination(
-                array(
-                  'mid_size' 	=> 1,
-                  'prev_text' => __( '&laquo;' ),
-                  'next_text' => __( '&raquo;' ),
-                  'screen_reader_text' => __( ' ' ),
-                )
-              );
-            ?>
+        <?php global $wp_query; if ( $wp_query->max_num_pages > 1 ): ?>
+          <div class="search-pagination">
+            <div class=" text-center">
+              <?php
+                the_posts_pagination(
+                  array(
+                    'mid_size' 	=> 1,
+                    'prev_text' => __( '&laquo;' ),
+                    'next_text' => __( '&raquo;' ),
+                    'screen_reader_text' => __( ' ' ),
+                  )
+                );
+              ?>
+            </div>
           </div>
-        </div>
+        <?php endif;?>
       </div>
       <div class="col-sm-4">
         <?php if( is_active_sidebar( 'wtp-archive-sidebar' ) ){ dynamic_sidebar( 'wtp-archive-sidebar' ); }?>
       </div>
     </div>
+  </div>
+  <div class="container-fluid">
+    <?php if( is_active_sidebar( 'single-post-footer' ) ){ dynamic_sidebar( 'single-post-footer' ); }?>
   </div>
 <?php get_footer();?>
