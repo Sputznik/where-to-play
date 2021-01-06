ef21a6333d4c9178b5197d895e0f1212eb98e19e
psychome004 <anil.stephen66@gmail.com>
2021-01-06 17:29:45 +0530

    Updated blog-post template

--- partials/blog-post.php
+++ partials/blog-post.php
@@ -7,8 +7,8 @@
     <div class="post-content">
       <h2 class="title"><?php the_title();?></h2>
       <div class='post-meta'><?php the_author(); echo ', '.get_the_date('M j, Y');?></div>
-      <?php echo do_shortcode('[blog_terms]');?>
       <div class="desc"><?php echo excerpt( 40 ); ?></div>
+      <?php echo do_shortcode('[blog_terms]');?>
     </div>
   </div>
 </a>
