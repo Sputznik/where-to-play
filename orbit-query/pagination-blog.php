0f28845640c2dc37ca7ac5e59e3c40c4ebc9f2eb
unknown <>
2021-01-25 13:58:45 +0530

    Fix pagination

--- orbit-query/pagination-blog.php
+++ orbit-query/pagination-blog.php
@@ -3,11 +3,11 @@
   $paged = ( get_query_var('orbit-paged')) ? get_query_var('orbit-paged') : 1;
 
   if( $this->query->max_num_pages > 1 ){
-    $current_page = max( 1, $paged );
+    $current_page = max( 1, get_query_var('paged') );
     $big = 9999999;
     echo paginate_links( array(
       'base'       => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-	    'format'     => '?orbit-paged=%#%',
+	    'format'     => '?paged=%#%',
       'type'       => 'list',
       'mid_size'   => 1,
       'current'    => $current_page,
