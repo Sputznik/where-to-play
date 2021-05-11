411b46b8a81ad26178209d5fc1c890989e90caf3
psychome004 <anil.stephen66@gmail.com>
2021-05-11 17:04:17 +0530

    Updated redirect_function

--- lib/cpt/cpt.php
+++ lib/cpt/cpt.php
@@ -107,7 +107,7 @@ function get_wtp_banners(){
 // REDIRECT SINGLE RESOURCE PAGE TO HOMEPAGE
 add_action( 'template_redirect', 'wtp_redirect_single_resource' );
 function wtp_redirect_single_resource(){
-  if ( is_singular( 'resource' ) && ( is_user_logged_in() || !is_user_logged_in()  ) ){
+  if ( is_singular( 'resource' ) &&  ! is_user_logged_in() ){
 		wp_redirect( home_url(), 302 );
 		exit();
 	}
